ee76093fcbfa71e6e936cd0da5fbc40135e88b63
Zachary Luna <zachluna@google.com>
2021-04-14 15:19:14 -0700

    file corrections

--- google/appengine/api/memcache/MemcacheUtils.php
+++ google/appengine/api/memcache/MemcacheUtils.php
@@ -23,6 +23,7 @@ namespace google\appengine\api\memcache;
 
 use google\appengine\MemcacheSetRequest;
 use google\appengine\MemcacheSetResponse;
+use google\appengine\runtime\ApiProxy;
 
 class MemcacheUtils {
 
