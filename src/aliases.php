6afd191d26525ee2fa406a2de248e8aa8a7c24f8
Zachary Luna <zachluna@google.com>
2021-05-04 10:17:52 -0700

    add alias.php description

--- src/aliases.php
+++ src/aliases.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Alias.php creates aliases for the classes to match the 
+ * original names used in the php55 sdk. This is for the
+ * convenience of migration to php7+ sdk. Alias.php is 
+ * included in the autoloader inside the composer.json.
+ */
+
 $classMap = [
     'Google\AppEngine\Api\AppIdentity\AppIdentityService' => 'google\appengine\api\appidentity\AppIdentityService'
 ];
