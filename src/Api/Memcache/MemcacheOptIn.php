9b716ba619c5c1944b0867a0e1e156aca5dd51f8
Zachary Luna <zachluna@google.com>
2021-05-12 14:17:18 -0700

    Add Opt-in Memcache file

---
+++ src/Api/Memcache/MemcacheOptIn.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return array(
+	'Memcache' => __DIR__ . 'src/Api/Memcache/Memcache.php',
+);
+// 'Memcache' => __DIR__ . 'Memcache.php';
+// 'Memcached' => __DIR__ . 'Memcached.php';
+
+// 'google/appengine/runtime/Memcached.php',
+// 'Memcache' => 'Google\AppEngine\Api\Memcache\Memcache',
