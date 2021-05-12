6395dfc6e39c61ca42603c0a123d3b46ea6cef4b
Zachary Luna <zachluna@google.com>
2021-05-12 16:55:10 -0700

    autoload testing

--- src/Api/Memcache/MemcacheOptIn.php
+++ src/Api/Memcache/MemcacheOptIn.php
@@ -1,37 +1,15 @@
 <?php
 
-
-// return array(
-// 	'Memcache' => __DIR__ . 'src/Api/Memcache/Memcache.php',
-// );
-
-// use 'Google\AppEngine\Api\Memcache\Memcache';
-
-// 'Memcache' => __DIR__ . 'Memcache.php';
-// 'Memcached' => __DIR__ . 'Memcached.php';
-
-// 'google/appengine/runtime/Memcached.php',
-// 'Memcache' => 'Google\AppEngine\Api\Memcache\Memcache',
-
-
-
-
-final class ClassLoader {
-	private static $classmap = null;
-	private static $sdk_root = null;
-
-	public static function loadClass($class_name) {
-	    self::$classmap = [
-	 		'Memcache' => __DIR__ . '/Memcache.php',
-	 		'Memcached' => __DIR__ . '/Memcached.php'
-	    ];
-	  if (array_key_exists($class_name, self::$classmap)) {
-	    	$target_file = self::$classmap[$class_name];
-	  		require  $target_file;
-	  }
-
+public function loadClass($class_name) {
+	$classmap = [
+		'Memcache' => __DIR__ . '/Memcache.php',
+		'Memcached' => __DIR__ . '/Memcached.php'
+	];
+	if (array_key_exists($class_name, self::$classmap)) {
+		$target_file = self::$classmap[$class_name];
+	  	require_once $target_file;
 	}
 }
 
- spl_autoload_register('\ClassLoader::loadClass');
+spl_autoload_register('loadClass');
 
