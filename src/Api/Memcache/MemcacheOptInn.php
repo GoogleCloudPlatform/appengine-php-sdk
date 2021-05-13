89fb084afa29d10c61e55349d41dac1acc4cfdd7
Zachary Luna <zachluna@google.com>
2021-05-13 12:03:31 -0700

    autoload testing

--- src/Api/Memcache/MemcacheOptInn.php
+++ src/Api/Memcache/MemcacheOptInn.php
@@ -8,7 +8,7 @@
  */
 
 $classMap = [
-    'Google\AppEngine\Api\Memcache\Memcache' => 'Memcache'
+    'Google\AppEngine\Api\Memcache\Memcache' => 'Memcache',
     'Google\AppEngine\Api\Memcache\Memcached' => 'Memcached'
 ];
 
