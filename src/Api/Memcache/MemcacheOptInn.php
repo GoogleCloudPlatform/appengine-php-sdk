db0073402d93bd4f1861db0aa4d8865dd1b2823e
Zachary Luna <zachluna@google.com>
2021-05-13 12:02:20 -0700

    autoload testing

---
+++ src/Api/Memcache/MemcacheOptInn.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Alias.php creates aliases for the classes to match the 
+ * original names used in the php55 sdk. This is for the
+ * convenience of migration to php7+ sdk. Alias.php is 
+ * included in the autoloader inside the composer.json.
+ */
+
+$classMap = [
+    'Google\AppEngine\Api\Memcache\Memcache' => 'Memcache'
+];
+
+foreach ($classMap as $class => $alias) {
+    class_alias($class, $alias);
+}
