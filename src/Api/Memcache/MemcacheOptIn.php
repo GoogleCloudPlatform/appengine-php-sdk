c99aad9f419376eccb6b1a2e4522be5dd991b1bb
Zachary Luna <zachluna@google.com>
2021-05-13 13:01:58 -0700

    autoload testing

--- src/Api/Memcache/MemcacheOptIn.php
+++ src/Api/Memcache/MemcacheOptIn.php
@@ -1,16 +1,18 @@
 <?php
 
-function loadClass($class_name) {
-	$classmap = [
-		'memcached' => __DIR__ . '/Memcached.php',
-		'memcache' => __DIR__ . '/Memcache.php'
-	];
-	$class_name = strtolower($class_name);
-	if (array_key_exists($class_name, $classmap)) {
-		$target_file = $classmap[$class_name];
-	  	require_once $target_file;
-	}
+/**
+ * MemcacheOptIn.php creates aliases for the classes to match the 
+ * original behavior used in the Memcache php55 sdk. This is for the
+ * convenience of migration to php7+ sdk. To opt in, please add 
+ * "src/Api/Memcache/MemcacheOptIn.php" to the autoload::files section of the 
+ * composer.json. 
+ */
+
+$classMap = [
+    'Google\AppEngine\Api\Memcache\Memcache' => 'Memcache',
+    'Google\AppEngine\Api\Memcache\Memcached' => 'Memcached'
+];
+
+foreach ($classMap as $class => $alias) {
+    class_alias($class, $alias);
 }
-
-spl_autoload_register('loadClass');
-
