f4beee60d0fd82ae5370330684c288e2ca12093f
Zachary Luna <zachluna@google.com>
2021-05-13 10:06:32 -0700

    autoload testing

--- src/Api/Memcache/MemcacheOptIn.php
+++ src/Api/Memcache/MemcacheOptIn.php
@@ -2,10 +2,10 @@
 
 function loadClass($class_name) {
 	$classmap = [
-		'Memcache' => __DIR__ . '/Memcache.php',
-		'Memcached' => __DIR__ . '/Memcached.php',
-		'memcached' => __DIR__ . '/Memcached.php'
+		'memcached' => __DIR__ . '/Memcached.php',
+		'memcache' => __DIR__ . '/Memcache.php'
 	];
+	$class_name = strtolower($class_name);
 	if (array_key_exists($class_name, $classmap)) {
 		$target_file = $classmap[$class_name];
 	  	require_once $target_file;
