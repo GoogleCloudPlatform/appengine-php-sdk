bfd7589aa857ca562a17b9974e420c0e191dca83
Zachary Luna <zachluna@google.com>
2021-05-19 16:44:23 -0700

    nit change

--- src/Api/UrlFetch/UrlFetchStream.php
+++ src/Api/UrlFetch/UrlFetchStream.php
@@ -18,7 +18,7 @@ declare(strict_types=1);
  */
 
 
-namespace Google\AppEngine\Api\Urlfetch;
+namespace Google\AppEngine\Api\UrlFetch;
 
 use Google\Appengine\Runtime\ApplicationError;
 use google\appengine\URLFetchServiceError\ErrorCode;
