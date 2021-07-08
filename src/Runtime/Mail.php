38f6aaa29190e7b47446bcff491170cd56cbd282
Zachary Luna <zachluna@google.com>
2021-07-08 12:56:41 -0700

    update

--- src/Runtime/Mail.php
+++ src/Runtime/Mail.php
@@ -50,73 +50,72 @@ function mail($to,
               $message,
               $additional_headers = [],
               $additional_parameters = "") {
-  echo "in php script\n";
-  // $raw_mail = "To: $to\r\nSubject: $subject\r\n";
-  // if ($additional_headers != null) {
-  //   $raw_mail .= trim($additional_headers);
-  // }
-  // $raw_mail .= "\r\n\r\n$message";
+  $raw_mail = "To: $to\r\nSubject: $subject\r\n";
+  if ($additional_headers != null) {
+    $raw_mail .= trim($additional_headers);
+  }
+  $raw_mail .= "\r\n\r\n$message";
 
-  // $mime = mailparse_msg_create();
-  // mailparse_msg_parse($mime, $raw_mail);
-  // $root_part = mailparse_msg_get_part_data($mime);
+  $mime = mailparse_msg_create();
+  mailparse_msg_parse($mime, $raw_mail);
+  $root_part = mailparse_msg_get_part_data($mime);
 
-  // // Set sender address based on the following order
-  // // 1. "From" header in $additional_headers
-  // // 2. "sendmail_from" ini setting
-  // // 3. Default address "mailer@<app-id>.appspotmail.com
-  // $from = ini_get('sendmail_from');
-  // if (isset($root_part['headers']['from'])) {
-  //   $from = $root_part['headers']['from'];
-  // }
-  // if ($from === false || $from == "") {
-  //   $from = sprintf('mailer@%s.appspotmail.com',
-  //                   AppIdentityService::getApplicationId());
-  //   syslog(LOG_WARNING,
-  //          "mail(): Unable to determine sender's email address from the " .
-  //          "'sendmail_from' directive in php.ini or from the 'From' " .
-  //          "header. Falling back to the default $from.");
-  // }
+  // Set sender address based on the following order
+  // 1. "From" header in $additional_headers
+  // 2. "sendmail_from" ini setting
+  // 3. Default address "mailer@<app-id>.appspotmail.com
+  $from = ini_get('sendmail_from');
+  if (isset($root_part['headers']['from'])) {
+    $from = $root_part['headers']['from'];
+  }
+  if ($from === false || $from == "") {
+    $from = sprintf('mailer@%s.appspotmail.com',
+                    AppIdentityService::getApplicationId());
+    syslog(LOG_WARNING,
+           "mail(): Unable to determine sender's email address from the " .
+           "'sendmail_from' directive in php.ini or from the 'From' " .
+           "header. Falling back to the default $from.");
+  }
 
-  // $email = new Message();
-  // try {
-  //   $email->setSender($from);
-  //   $email->addTo($root_part['headers']['to']);
-  //   if (isset($root_part['headers']['cc'])) {
-  //     $email->AddCc($root_part['headers']['cc']);
-  //   }
-  //   if (isset($root_part['headers']['bcc'])) {
-  //     $email->AddBcc($root_part['headers']['bcc']);
-  //   }
-  //   if (isset($root_part['headers']['reply-to'])) {
-  //     $email->setReplyTo($root_part['headers']['reply-to']);
-  //   }
-  //   $email->setSubject($root_part['headers']['subject']);
-  //   $parts = mailparse_msg_get_structure($mime);
-  //   if (count($parts) > 1) {
-  //     foreach ($parts as $part_id) {
-  //       $part = mailparse_msg_get_part($mime, $part_id);
-  //       parseMimePart($part, $raw_mail, $email);
-  //     }
-  //   } else if ($root_part['content-type'] == 'text/plain') {
-  //     $email->setTextBody($message);
-  //   }  else if ($root_part['content-type'] == 'text/html') {
-  //     $email->setHtmlBody($message);
-  //   }
-  //   $extra_headers = array_diff_key($root_part['headers'], array_flip([
-  //       'from', 'to', 'cc', 'bcc', 'reply-to', 'subject', 'content-type']));
-  //   foreach ($extra_headers as $key => $value) {
-  //     try {
-  //       $email->addHeader($key, $value);
-  //     } catch (\InvalidArgumentException $e) {
-  //       syslog(LOG_WARNING, "mail:() Dropping disallowed email header $key");
-  //     }
-  //   }
-  //   $email->send();
-  // } catch (\Exception $e) {
-  //   trigger_error('mail(): ' . $e->getMessage(), E_USER_WARNING);
-  //   return false;
-  // }
+  $email = new Message();
+  try {
+    $email->setSender($from);
+    $email->addTo($root_part['headers']['to']);
+    if (isset($root_part['headers']['cc'])) {
+      $email->AddCc($root_part['headers']['cc']);
+    }
+    if (isset($root_part['headers']['bcc'])) {
+      $email->AddBcc($root_part['headers']['bcc']);
+    }
+    if (isset($root_part['headers']['reply-to'])) {
+      $email->setReplyTo($root_part['headers']['reply-to']);
+    }
+    $email->setSubject($root_part['headers']['subject']);
+    $parts = mailparse_msg_get_structure($mime);
+    if (count($parts) > 1) {
+      foreach ($parts as $part_id) {
+        $part = mailparse_msg_get_part($mime, $part_id);
+        parseMimePart($part, $raw_mail, $email);
+      }
+    } else if ($root_part['content-type'] == 'text/plain') {
+      $email->setTextBody($message);
+    }  else if ($root_part['content-type'] == 'text/html') {
+      $email->setHtmlBody($message);
+    }
+    $extra_headers = array_diff_key($root_part['headers'], array_flip([
+        'from', 'to', 'cc', 'bcc', 'reply-to', 'subject', 'content-type']));
+    foreach ($extra_headers as $key => $value) {
+      try {
+        $email->addHeader($key, $value);
+      } catch (\InvalidArgumentException $e) {
+        syslog(LOG_WARNING, "mail:() Dropping disallowed email header $key");
+      }
+    }
+    $email->send();
+  } catch (\Exception $e) {
+    trigger_error('mail(): ' . $e->getMessage(), E_USER_WARNING);
+    return false;
+  }
 
   return true;
 }
