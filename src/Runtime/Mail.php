38aeba87f6e41dfac7021996dd94b093c339fdd3
Zachary Luna <zachluna@google.com>
2021-07-11 18:52:38 -0700

    edit

--- src/Runtime/Mail.php
+++ src/Runtime/Mail.php
@@ -32,6 +32,7 @@ require_once __DIR__ . "/../Api/Mail/BaseMessage.php";
 require_once __DIR__ . "/../Api/Mail/Message.php";
 require_once __DIR__ . "/../Runtime/ApiProxyBase.php";
 require_once __DIR__ . "/../Runtime/ApplicationError.php";
+require_once __DIR__ . "/../Runtime/Error.php";
 require_once __DIR__ . "/../Runtime/VmApiProxy.php";
 require_once __DIR__ . "/../Runtime/ApiProxy.php";
 require_once __DIR__ . "/../Ext/RemoteApi/remote_api_pb.php";
