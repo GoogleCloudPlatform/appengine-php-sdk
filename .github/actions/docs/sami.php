6d48da2a8888171cf50504daa03927adcca1e07b
Zachary Luna <zachluna@google.com>
2021-08-09 19:02:09 -0700

    Testing Changes

--- .github/actions/docs/sami.php
+++ .github/actions/docs/sami.php
@@ -1,6 +1,6 @@
 <?php
 
-// require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
 use Sami\RemoteRepository\GitHubRemoteRepository;
 use Sami\Sami;
